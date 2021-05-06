aee95c22b99d2fa2e296457a945f29670565618e
hans0 <kiessler.hans@gmail.com>
2021-05-06 13:59:42 -0400

    frontend deployment, attempt 2

--- index.js
+++ index.js
@@ -2,27 +2,20 @@ console.log("it is working");
 
 require('dotenv').config()
 const path = require('path')
-
-const express = require('express');
-
-const server = express();
-
-const PORT = process.env.PORT || 5002; 
-
+const express = require('express')
+const server = express()
+const PORT = process.env.PORT || 5001
+process.env.API_KEY || 'gaga'
 server.use(express.static(path.join(__dirname, 'client/build')))
-
-server.use(express.json());
-
+server.use(express.json())
 server.get('/api/*', (req, res) => {
   res.json({
-    cohort: 'Web 41'
+    cohort: 'Web 41',
   })
-});
-
-server.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 })
-
+server.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
 server.listen(PORT, () => {
   console.log(`listening on ${PORT}`)
 })
